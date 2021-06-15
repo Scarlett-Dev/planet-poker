f4f915720600fb54220092c1bedd31527a7fecba
Scarlett <scarlettdevacc@gmail.com>
2021-06-15 22:03:29 +0200

    Added todo's for emit and listen for specific events.
    Code currently works with the plugin: Moesif Origin & CORS Changer
    (Workaround since this issue (CORS) will not occur in Deployment)

--- planet-poker/index.js
+++ planet-poker/index.js
@@ -6,18 +6,22 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+var corsOptions = {
+  origin: 'http://localhost/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 app.use(cors());
+console.log('CORS USED: ', cors());
+// app.use((req,res,next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH,PUT, DELETE, OPTIONS");
+//   next();
+// });
 
-app.use((req,res,next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH,PUT, DELETE, OPTIONS");
-  next();
-});
-
-app.get('/', (req, res) => {
+app.get('/', cors(corsOptions), (req, res) => {
   res.sendFile(__dirname + '/src/index.html');
-  
 });
 
 // app.get('/io', (req, res) => {
--- planet-poker/src/app/observer/observer.component.ts
+++ planet-poker/src/app/observer/observer.component.ts
@@ -23,20 +23,37 @@ private messageText = "";
 
        ngOnInit() {
         this.messages = new Array();
+        console.log('STARTED:', this.messages);
+
+        //Todo: Figure out how to emit an event (Broadcast?)
+        this.socket.emit('potato', () => {
+            this.messages.push('Start_Potato')
+            console.log('Emit The Potato!');
+        });
+
+        //TODO: Figure our how to react to an event (Listener?)
+        this.socket.on('potato', () => {
+            console.log('Potato has entered the virtual env!')
+        });
 
         this.socket.on('message-received', (msg: any) => {
             this.messages.push(msg);
             console.log(msg);
             console.log(this.messages);
         });
+
       this.socket.emit('event1', {
           msg: 'Client to server, can you hear me server?'
       });
-      this.socket.on('event2', (data: any) => {
+
+      this.socket.on('event1', (data: any) => {
+        console.log('Event 1 emitted.')
+
         console.log(data.msg);
         this.socket.emit('event3', {
             msg: 'Yes, its working for me!!'
         });
+
       });
       this.socket.on('event4', (data: any) => {
           console.log(data.msg);
